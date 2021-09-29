a57856c50422e774622ad3a455974744838b59c4
Paulo Barbosa <paulobarbosa673@gmail.com>
2021-09-28 21:41:37 -0400

    :tada: Desafio concluído

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,18 @@
-export function SideBar() {
-  // Complete aqui
+import { ReactNode } from "react";
+
+import '../styles/sidebar.scss';
+
+interface SideBarProps {
+  children: ReactNode;
+}
+
+export function SideBar({ children }: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+      <div className="buttons-container">{children}</div>
+    </nav>
+  );
 }
